67dfcd41607f5b7e4a9dda71cc604fe3d5f0aa4d
Kseniya <zyaboed@gmail.com>
2019-07-29 14:05:15 +0300

    6 урок уровень нормал

---
+++ 06_normal.py
@@ -0,0 +1,58 @@
+# Задание-1:
+# Реализуйте описаную ниже задачу, используя парадигмы ООП:
+# В школе есть Классы(5А, 7Б и т.д.), в которых учатся Ученики.
+# У каждого ученика есть два Родителя(мама и папа).
+# Также в школе преподают Учителя. Один учитель может преподавать 
+# в неограниченном кол-ве классов свой определенный предмет. 
+# Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
+# но больше математику не может преподавать никто другой.
+
+# Выбранная и заполненная данными структура должна решать следующие задачи:
+# 1. Получить полный список всех классов школы
+# 2. Получить список всех учеников в указанном классе
+#  (каждый ученик отображается в формате "Фамилия И.О.")
+# 3. Получить список всех предметов указанного ученика 
+#  (Ученик --> Класс --> Учителя --> Предметы)
+# 4. Узнать ФИО родителей указанного ученика
+# 5. Получить список всех Учителей, преподающих в указанном классе
+
+class School:
+    def __init__(self, school_name, school_adress, teachers, students):
+        self._school_name = school_name
+        self._school_adress = school_adress
+        self._teachers = teachers
+        self._students = students
+
+    def get_all_classes(self):
+        classes = set([student.get_class_room for student in self._students])
+        return list(sorted(classes, key = lambda x: int(x[:-1])))
+
+    def get_students(self, class_room):
+        return [student.get_short_name for student in self._students if
+                class_room == student.get_class_room]
+
+    def get_teachers(self, class_room):
+        return [teacher.get_short_name for teacher in self._teachers if
+                class_room in teacher.get_classes]
+
+    def find_student(self, student_full_name):
+        for person in self._students:
+            if student_full_name == person.get_full_name:
+                teachers = [teachers.get_short_name for teachers in
+                            self._teachers if person.get_class_room in
+                            teachers.get_classes]
+                lessons = [teachers.get_courses for teachers in
+                           self._teachers if person.get_class_room in
+                           teachers.get_classes]
+                parents = person.get_parents
+
+                return {
+                    'full_name': student_full_name,
+                    'class_room': person.get_class_room,
+                    'teachers': teachers,
+                    'lessons': lessons,
+                    'parents': parents
+                    }
+
+
+
